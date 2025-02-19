c64277fdf5810b5bc3cf194c7ef22c442110bc93
Tyler <ty@zzox.net>
2025-02-19 15:34:29 -0600

    bring over colors

---
+++ src/ui/colors.ts
@@ -0,0 +1,43 @@
+enum Colors {
+  // from solarized
+  WHITE = 0xfdf6e3,
+  OFFWHITE = 0xeee8d5,
+  GREEN = 0x859900,
+  GREY = 0x93a1a1,
+  DARKGREY = 0x586e75,
+  ORANGE = 0xcb4b16,
+  PURPLE = 0x6c71c4,
+  BLUE = 0x268bd2,
+  EMERALD = 0x2aa198,
+  TAN = 0xb58900,
+
+  // made up
+  BG = 0x0e0e0e,
+  BROWN = 0x893203,
+  GOLD = 0xd8df23,
+
+  // Not yet used made up colors
+  DARK_GREEN = 0x587558,
+}
+
+// needed?
+const paletteColors = [
+  Colors.WHITE,
+  Colors.OFFWHITE,
+  Colors.GREEN,
+  Colors.GREY,
+  Colors.DARKGREY,
+  Colors.ORANGE,
+  Colors.PURPLE,
+  Colors.BLUE,
+  Colors.EMERALD,
+  Colors.TAN,
+
+  // made up
+  Colors.BG,
+  Colors.BROWN,
+  Colors.GOLD,
+
+  // Not yet used colors
+  Colors.DARK_GREEN,
+];
