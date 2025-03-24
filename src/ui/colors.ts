a4f5f25b06f4dd454878fe0b7d029f7a3ca508ac
zzox <tweedlitweedli@gmail.com>
2025-03-23 21:48:02 -0500

    colors file uses enum

--- src/ui/colors.ts
+++ src/ui/colors.ts
@@ -1,70 +1,89 @@
-enum Colors {
-  // from solarized
-  WHITE = 0xfdf6e3,
-  WHITESMOKE = 0xf5f5f5,
-  OFFWHITE = 0xeee8d5,
-  GREEN = 0x859900,
-  GREY = 0x93a1a1,
-  DARKGREY = 0x586e75,
-  ORANGE = 0xcb4b16,
-  PURPLE = 0x6c71c4,
-  BLUE = 0x268bd2,
-  EMERALD = 0x2aa198,
-  TAN = 0xb58900,
+export enum Color {
+  // made up
+  Bg = 'BG',
+
+  Whtie = 'WHITE',
+  WhiteSmoke = 'WHITESMOKE',
+  OffWhite = 'OFFWHITE',
+  Green = 'GREEN',
+  Grey = 'GREY',
+  DarkGrey = 'DARKGREY',
+  Orange = 'ORANGE',
+  Purple = 'PURPLE',
+  Blue = 'BLUE',
+  Emerald = 'EMERALD',
+  Tan = 'TAN',
 
   // made up
-  BG = 0x0e0e0e,
-  BROWN = 0x893203,
-  GOLD = 0xd8df23,
-
-  // Not yet used made up colors
-  DARK_GREEN = 0x587558,
-}
-
-const paletteColors = [
-  // made up
-  Colors.BG,
-
-  Colors.WHITE,
-  Colors.WHITESMOKE,
-  Colors.OFFWHITE,
-  Colors.GREEN,
-  Colors.GREY,
-  Colors.DARKGREY,
-  Colors.ORANGE,
-  Colors.PURPLE,
-  Colors.BLUE,
-  Colors.EMERALD,
-  Colors.TAN,
-
-  // made up
-  Colors.BROWN,
-  Colors.GOLD,
-
-  // // Not yet used colors
-  // Colors.DARK_GREEN,
-]
-
-export const cssColors = [
-  // made up
-  'BG',
-
-  'WHITE',
-  'WHITESMOKE',
-  'OFFWHITE',
-  'GREEN',
-  'GREY',
-  'DARKGREY',
-  'ORANGE',
-  'PURPLE',
-  'BLUE',
-  'EMERALD',
-  'TAN',
-
-  // made up
-  'BROWN',
-  'GOLD',
+  Brown = 'BROWN',
+  Gold = 'GOLD',
 
   // Not yet used colors
-  'DARK_GREEN',
+  DarkGreen = 'DARK_GREEN',
+}
+
+export const cssColors = [
+  Color.Bg,
+  Color.Whtie,
+  Color.WhiteSmoke,
+  Color.OffWhite,
+  Color.Green,
+  Color.Grey,
+  Color.DarkGrey,
+  Color.Orange,
+  Color.Purple,
+  Color.Blue,
+  Color.Emerald,
+  Color.Tan,
+  Color.Brown,
+  Color.Gold,
+  Color.DarkGreen
 ]
+
+// not using the following colors, we just use css
+// enum Colors {
+//   // from solarized
+//   WHITE = 0xfdf6e3,
+//   WHITESMOKE = 0xf5f5f5,
+//   OFFWHITE = 0xeee8d5,
+//   GREEN = 0x859900,
+//   GREY = 0x93a1a1,
+//   DARKGREY = 0x586e75,
+//   ORANGE = 0xcb4b16,
+//   PURPLE = 0x6c71c4,
+//   BLUE = 0x268bd2,
+//   EMERALD = 0x2aa198,
+//   TAN = 0xb58900,
+
+//   // made up
+//   BG = 0x0e0e0e,
+//   BROWN = 0x893203,
+//   GOLD = 0xd8df23,
+
+//   // Not yet used made up colors
+//   DARK_GREEN = 0x587558,
+// }
+
+// const paletteColors = [
+//   // made up
+//   Colors.BG,
+
+//   Colors.WHITE,
+//   Colors.WHITESMOKE,
+//   Colors.OFFWHITE,
+//   Colors.GREEN,
+//   Colors.GREY,
+//   Colors.DARKGREY,
+//   Colors.ORANGE,
+//   Colors.PURPLE,
+//   Colors.BLUE,
+//   Colors.EMERALD,
+//   Colors.TAN,
+
+//   // made up
+//   Colors.BROWN,
+//   Colors.GOLD,
+
+//   // // Not yet used colors
+//   // Colors.DARK_GREEN,
+// ]
